30b87fe2076d9f76273ed30df5ab0a0f48722193
yagnaa3 <g2zc57r487@privaterelay.appleid.com>
2026-09-24 21:55:36 +0530

    Modify main function to check for lowercase character
    Replaced integer maximum calculation with character input check.

--- ASSIGNMENT_1/19.c
+++ ASSIGNMENT_1/19.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-    int a,b,c,max;
-    printf("Enter three integers: ");
-    scanf("%d %d %d", &a, &b, &c);
+    char ch;
 
-    max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-    printf("Maximum is: %d\n", max);
+    printf("enter a character: ");
+    scanf("%c", &ch);
+
+    (ch >= 'a' && ch <= 'z') ? printf("it is small letter\n") : printf("not a small letter\n");
 }
