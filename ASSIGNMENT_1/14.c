3ea7591d8874fe1e35bc6ff1d7da3c181f3c3a7b
yagnaa3 <g2zc57r487@privaterelay.appleid.com>
2026-09-24 21:55:03 +0530

    Update 14.c

--- ASSIGNMENT_1/14.c
+++ ASSIGNMENT_1/14.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+
 int main()
 {
     char ch;
-    printf("Enter any character");
-    scanf("%c",ch);
 
-   if(ch >= 'A' && ch <= 'Z')
-   {
-    printf("The chqaracter %c is a big case letter\n",ch);
-   }
-   else if(ch >= 'a' && ch <= 'z')
-   {
-    printf("The chqaracter %c is a small case letter\n",ch);
-   }
-   else if(ch >= '1' && ch <= '9')
-   {
-    printf("The chqaracter %c is a digit\n",ch);
-   }
-   else
-   {
-    printf("The chqaracter %c is a special symbol\n",ch);
-   }
+    printf("enter any character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        printf("capital letter\n");
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("small letter\n");
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+        printf("digit\n");
+    }
+    else
+    {
+        printf("special symbol\n");
+    }
 }
